d89d26a8107d511294567ef9db3a5ed371d6ee2c
Junior Vengrzynek <junior.vzk@gmail.com>
2018-01-04 10:12:29 -0200

    prod

--- expo_salute/components/Products/Products.js
+++ expo_salute/components/Products/Products.js
@@ -8,7 +8,8 @@ export default class Products extends Component {
   constructor(props){
     super(props);
     this.state = {
-      products:[]
+      products:[],
+      order:[]
     }
   }
 
@@ -33,16 +34,42 @@ export default class Products extends Component {
 
   }
 
+
+
+
   _renderHeader(section) {
     return (
       <View style={styles.header}>
 
       <Image style={styles.headerPhoto} source={{ uri: section.image }} />
       <Text style={styles.headerText}>{section.name}</Text>
+      <TouchableOpacity style={styles.headerButton} onPress={ ()=> {
+
+        this.setState(previousState => {
+          var newOrder = this.state.order;
+          var index = newOrder.indexOf(section.id);
+
+          if( index == -1 ){
+            newOrder[newOrder.length] =
+          } else{
+
+          }
+
+          return { order:  };
+        });
+
+      } }>
+        <Text style={ {fontSize:30, aligntText: 'center',color:'white' } }>-</Text>
+      </TouchableOpacity>
+      <Text> { ()=> {
+
+
+       } } </Text>
       <TouchableOpacity style={styles.headerButton} onPress={ ()=> {alert("Fui clicado!")} }>
         <Text style={ {fontSize:30, aligntText: 'center',color:'white' } }>+</Text>
       </TouchableOpacity>
       </View>
+
     );
   }
 
@@ -50,6 +77,9 @@ export default class Products extends Component {
     return (
       <View style={styles.content}>
         <Text style={styles.contentText}>{section.description}</Text>
+        <TouchableOpacity style={styles.sendOrderButton} onPress={ ()=> {alert("Fui clicado!")} }>
+          <Text style={ {fontSize:30, aligntText: 'center',color:'white' } }>+</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -83,17 +113,20 @@ const styles = StyleSheet.create({
     alignItems:'center',
     justifyContent:'flex-start'
   },
+
   headerPhoto:{
     height:50,
     width: 50,
     marginRight:10,
     borderRadius: 100
   },
+
   headerText:{
     flex:4,
     fontSize:20,
 
   },
+
   headerButton:{
     flex:1,
     flexDirection: 'row',
@@ -104,10 +137,12 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     backgroundColor: 'rgba(44, 62, 80,1.0)',
   },
+
   content:{
     paddingLeft: 50,
     paddingRight: 30,
   },
+
   contentText:{
     fontSize: 16
   },
